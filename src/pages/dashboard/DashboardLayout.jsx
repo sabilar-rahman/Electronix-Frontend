e1aefeea29354d80304e201aef6d2b193e2e5852
sabilar-rahman <sabilarrahman36@gmail.com>
2024-12-16 05:13:50 +0600

    Dashboard Layout

--- src/pages/dashboard/DashboardLayout.jsx
+++ src/pages/dashboard/DashboardLayout.jsx
@@ -1,12 +1,35 @@
-import { Outlet } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { Navigate, Outlet } from "react-router-dom";
 
 
 const DashboardLayout = () => {
+
+    const { user } = useSelector((state) => state.auth);
+
+    if (!user) {
+        return <Navigate to="/login" replace />
+    }
+
+    const renderDashboard = () => {
+        switch (user?.role) {
+            case "admin":
+                return <div>admin dashboard</div>
+            case "user":
+                return <div>user dashboard</div>
+            default:
+                return <Navigate to="/login" replace />
+        }
+    }
     return (
-        <div>
-            <h1>Dashboard Layout</h1>
-            <Outlet/>
-            
+        <div className="container mx-auto flex flex-col md:flex-row gap-4 items-start justify-start">
+            <header className="lg:w-1/5 sm:2/5 w-full border">Header</header>
+
+            {renderDashboard()}
+
+            <main className="p-8 w-full border mt-4"></main>
+
+            <Outlet />
+
         </div>
 
     );
